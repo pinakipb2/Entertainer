b56967459be78a057eeb96e068428d3db2387453
Pinaki Bhattacharjee <pinakipb2@gmail.com>
2022-11-05 15:46:41 +0530

    Fixed styling

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -13,44 +13,44 @@ const Table = ({ data, setWatching, setWishlisted, setCompleted }) => {
           <div className="overflow-hidden text-center">
             {data.length > 0 ? (
               <table className="min-w-full">
-                <thead className="border-b">
+                <thead className="border-b font-bold">
                   <tr className="w-full">
-                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-3">
+                    <th scope="col" className="text-sm text-gray-900 px-6 py-3">
                       #
                     </th>
-                    <th scope="col" className="w-1/2 text-sm font-medium text-gray-900 px-6 py-3">
+                    <th scope="col" className="text-sm text-gray-900 px-6 py-3">
                       Entertainment Name
                     </th>
-                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-3">
+                    <th scope="col" className="text-sm text-gray-900 px-6 py-3">
                       Season
                     </th>
-                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-3">
+                    <th scope="col" className="text-sm text-gray-900 px-6 py-3">
                       Episode
                     </th>
-                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-3">
+                    <th scope="col" className="text-sm text-gray-900 px-6 py-3">
                       Created At
                     </th>
-                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-3">
+                    <th scope="col" className="text-sm text-gray-900 px-6 py-3">
                       Updated At
                     </th>
-                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-3">
+                    <th scope="col" className="text-sm text-gray-900 px-6 py-3">
                       Link
                     </th>
-                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-3">
+                    <th scope="col" className="text-sm text-gray-900 px-6 py-3">
                       Update
                     </th>
                   </tr>
                 </thead>
                 <tbody>
                   {data.map((r, idx) => (
-                    <tr className="border-b" key={r.id}>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{idx + 1}</td>
-                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{r.entertainmentName}</td>
-                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{r.season}</td>
-                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{r.episode}</td>
-                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{new Date(r.createdAt).toLocaleString('hi')}</td>
-                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{new Date(r.updatedAt).toLocaleString('hi')}</td>
-                      <td className={`text-sm font-light px-6 py-4 whitespace-nowrap ${r.link !== '' ? 'hover:cursor-pointer text-gray-900' : 'hover:cursor-not-allowed text-gray-400'}`}>
+                    <tr className="border-b text-gray-600" key={r.id}>
+                      <td className="px-6 py-3 whitespace-nowrap text-sm font-medium">{idx + 1}</td>
+                      <td className="text-sm font-light px-3 py-3 whitespace-nowrap">{r.entertainmentName}</td>
+                      <td className="text-sm font-light px-6 py-3 whitespace-nowrap">{r.season}</td>
+                      <td className="text-sm font-light px-6 py-3 whitespace-nowrap">{r.episode}</td>
+                      <td className="text-sm font-light px-6 py-3 whitespace-nowrap">{new Date(r.createdAt).toLocaleString('hi')}</td>
+                      <td className="text-sm font-light px-6 py-3 whitespace-nowrap">{new Date(r.updatedAt).toLocaleString('hi')}</td>
+                      <td className={`text-sm font-light px-6 py-3 whitespace-nowrap ${r.link !== '' ? 'hover:cursor-pointer text-gray-900' : 'hover:cursor-not-allowed text-gray-400'}`}>
                         {r.link === '' ? (
                           <FiExternalLink size={20} />
                         ) : (
@@ -59,7 +59,7 @@ const Table = ({ data, setWatching, setWishlisted, setCompleted }) => {
                           </a>
                         )}
                       </td>
-                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                      <td className="text-sm text-gray-900 font-light px-6 py-3 whitespace-nowrap">
                         <button
                           className="bg-red-600 hover:bg-red-700 text-white p-2 rounded"
                           onClick={() => {
